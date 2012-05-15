ee4c2a9710562310a63ae7ab9555fe569f252673
Gene Boggs <gene@cpan.org>
2012-05-15 23:38:16 +0000

    Import of GENE/Graph-Weighted-0.51 from CPAN.
    gitpan-cpan-distribution: Graph-Weighted
    gitpan-cpan-version:      0.51
    gitpan-cpan-path:         GENE/Graph-Weighted-0.51.tar.gz
    gitpan-cpan-author:       GENE
    gitpan-cpan-maturity:     released

--- lib/Graph/Weighted.pm
+++ lib/Graph/Weighted.pm
@@ -1,9 +1,10 @@
 package Graph::Weighted;
+# ABSTRACT: A weighted graph implementation
 
 use warnings;
 use strict;
 
-our $VERSION = '0.5';
+our $VERSION = '0.51';
 
 use base qw(Graph);
 
@@ -40,31 +41,43 @@ sub _add_weighted_edges {
     warn "add_weighted_edges(): $vertex, $neighbors, $attr\n" if DEBUG;
 
     # Initial vertex weight
-    my $weight = 0;
+    my $vertex_weight = 0;
 
     # Make nodes and edges.
     for my $n (0 .. @$neighbors - 1) {
-        next unless $neighbors->[$n]; # Skip zero weight nodes.
-        warn "Edge: $vertex <-> $n\n" if DEBUG;
+        my $w = $neighbors->[$n]; # Weight of the edge to the neighbor.
+        next unless $w; # Skip zero weight nodes.
         # Add a node-node edge to the graph.
         $self->add_edge($vertex, $n);
-        # Do the heavy lilfting:
-        $weight = _compute_weight($neighbors->[$n], $weight, $method, $attr);
+        # Set the weight of the edge.
+        my $edge_weight = _compute_edge_weight($w, $method, $attr);
+        warn "Edge: $vertex -($edge_weight)-> $n\n" if DEBUG;
+        $self->set_edge_attribute($vertex, $n, $attr, $edge_weight);
+        # Tally the weight of the vertex.
+        $vertex_weight = _compute_vertex_weight($w, $vertex_weight, $method, $attr);
     }
 
     # Set the weight of the graph node.
-    warn "$vertex weight = $weight\n" if DEBUG;
-    # TODO Handle multi-attribute update (not just weight).
-    $self->set_vertex_attribute($vertex, $attr, $weight);
+    warn "Vertex $vertex $attr = $vertex_weight\n" if DEBUG;
+    $self->set_vertex_attribute($vertex, $attr, $vertex_weight);
 }
 
-sub _compute_weight {
-    my ($node_weight, $current, $method, $attr) = @_;
-    warn "compute_weight(): $node_weight, $current\n" if DEBUG;
+sub _compute_edge_weight {
+    my ($weight, $method, $attr) = @_;
+    warn "compute_edge_weight(): $attr $weight\n" if DEBUG;
     # Call the weight function if one is given.
-    return $method->($node_weight, $current, $attr) if $method and ref $method eq 'CODE';
+    return $method->($weight, $attr) if $method and ref $method eq 'CODE';
     # Increment the current value by the node weight if no weight function is given.
-    return $node_weight + $current;
+    return $weight;
+}
+
+sub _compute_vertex_weight {
+    my ($weight, $current, $method, $attr) = @_;
+    warn "compute_vertex_weight(): $attr $weight, $current\n" if DEBUG;
+    # Call the weight function if one is given.
+    return $method->($weight, $current, $attr) if $method and ref $method eq 'CODE';
+    # Increment the current value by the node weight if no weight function is given.
+    return $weight + $current;
 }
 
 sub get_weight {
@@ -73,11 +86,16 @@ sub get_weight {
     return $self->get_attr(@_);
 }
 sub get_attr {
-    my ($self, $vertex, $attr) = @_;
-    die 'ERROR: No vertex given to get_attr()' unless defined $vertex;
+    my ($self, $v, $attr) = @_;
+    die 'ERROR: No vertex given to get_attr()' unless defined $v;
     $attr ||= WEIGHT;
-warn"get_attr() $vertex, $attr\n" if DEBUG;
-    return $self->get_vertex_attribute($vertex, $attr);
+    warn"get_attr() @$v, $attr\n" if DEBUG;
+    if (ref $v eq 'ARRAY') {
+        return $self->get_edge_attribute(@$v, $attr);
+    }
+    else {
+        return $self->get_vertex_attribute($v, $attr);
+    }
 }
 
 1;
@@ -91,17 +109,28 @@ Graph::Weighted - A weighted graph implementation
 
   use Graph::Weighted;
   my $g = Graph::Weighted->new();
-  $g->populate(
-      [
-        [ 0, 1, 2, 0, 0 ],  # A vertex of weight 3 with two edges.
-        [ 1, 0, 3, 0, 0 ],  # A vertex of weight 4 with two edges.
-        [ 2, 3, 0, 0, 0 ],  # A vertex of weight 5 with two edges.
-        [ 0, 0, 1, 0, 0 ],  # A vertex of weight 1 with one edge.
-        [ 0, 0, 0, 0, 0 ]   # A vertex of weight 0 with no edges.
+  $g->populate( # weight
+      [ [ 0, 1, 2, 0, 0 ], # Vertex with 2 edges of weight 3
+        [ 1, 0, 3, 0, 0 ], # Vertex with 2 edges of weight 4
+        [ 2, 3, 0, 0, 0 ], # Vertex with 2 edges of weight 5
+        [ 0, 0, 1, 0, 0 ], # Vertex with 1 edge of weight 1
+        [ 0, 0, 0, 0, 0 ], # Vertex with no edges of weight 0
       ]
   );
-  my $weight = $g->get_weight($vertex);
-  my $magnitude = $g->get_attr($vertex, 'magnitude');
+  $g->populate( # magnitude
+      [ [ 0, 2, 1, 0, 0 ], # Vertex with 2 edges of weight 3
+        [ 3, 0, 1, 0, 0 ], # Vertex with 2 edges of weight 4
+        [ 3, 2, 0, 0, 0 ], # Vertex with 2 edges of weight 5
+        [ 0, 0, 2, 0, 0 ], # Vertex with 1 edge of weight 2
+        [ 1, 1, 1, 1, 0 ], # Vertex with 4 edges of weight 4
+      ]
+  );
+  my $vertex = 0;
+  my $vertex_weight = $g->get_weight($vertex); # 3
+  my $vertex_magnitude = $g->get_attr($vertex, 'magnitude'); # 3
+  my $edge = [0, 1];
+  my $edge_weight = $g->get_weight($edge); # 1
+  my $edge_magnitude = $g->get_attr($edge, 'magnitude'); # 2
 
 =head1 DESCRIPTION
 
@@ -121,26 +150,52 @@ See L<Graph> for the possible constructor arguments.
 
 =head2 populate($data, $method, $attribute)
 
-  data      => 2D ARRAYREF of numbers
+  data      => ARRAYREF of numeric vectors
   method    => Optional CODEREF weighting function
   attribute => Optional STRING
 
-Populate the graph with weighted nodes (attribute named 'weight' by
-default).
+Populate a graph with weighted nodes.
 
-The default weighting function is a simple sum of the neighbor weight
-values.  An alternative may be provided, which should accept arguments
-of the current node weight, current weight total and the attribute to
-update.  For example:
+Example of array reference nodes:
 
-  sub weight_function {
+    [] no vertices (vertex weight 0)
+    [0] 1 vertex and no edges (vertex weight 0)
+    [1] 1 vertex and 1 edge (vertex weight 1)
+    [0,1] 2 vertices and 1 edge (vertex weight 1)
+    [0,1,9] 3 vertices and 2 edges (vertex weight 10)
+
+The default edge weighting function returns the value in the neighbor
+position.  An alternative may be provided, which should accept
+arguments of the current edge weight and the attribute to update.  For
+example:
+
+  sub edge_weight_function {
+    my ($weight, attribute);
+    return $current_weight_total / $current_node_weight;
+  }
+
+The default vertex weighting function is a simple sum of the neighbor
+weight values.  An alternative may be provided, which should accept
+arguments of the current node weight, current weight total and the
+attribute to update.  For example:
+
+  sub vertex_weight_function {
     my ($current_node_weight, $current_weight_total, attribute);
     return $current_weight_total / $current_node_weight;
   }
 
+The attribute is named 'weight' by default but may be anything of
+your choosing.  This method can be called multiple times on the same
+graph, if the nodes have multiple attributes.
+
 =head2 get_weight($vertex) and get_attr($vertex, $attribute);
 
-Return the attribute value.
+  $g->get_weight($vertex);
+  $g->get_attr($vertex, $attribute);
+  $g->get_weight(@edge);
+  $g->get_attr(@edge, $attribute);
+
+Return the attribute value for the vertex or edge.
 
 =head1 TO DO
 
@@ -155,12 +210,6 @@ Make subroutines for finding the total weight beneath a node.
 L<Graph>
 
 The F<t/*> sources.
-
-=head1 TO DO
-
-Handle hashref data.
-
-Handle Matrix::* objects as data.
 
 =head1 AUTHOR
 
--- t/01-populate.t
+++ t/01-populate.t
@@ -7,31 +7,41 @@ BEGIN {
 };
 
 my $weight_dataset = [
-    [],                   # No nodes
+    [], [[]],             # No nodes
     [ [ 0 ], ],           # 1 node, no edges
-    [ [ 1, 0, ],          # 2 nodes w=1,1 of self-edges
+    [ [ 1, 0, ],          # 2 nodes of "self-edges"
       [ 0, 1, ], ],
-    [ [ 0, 1, 2, 0, 0, ], # 5 nodes w=3,4,5,1,0
-      [ 1, 0, 3, 0, 0, ],
-      [ 2, 3, 0, 0, 0, ],
-      [ 0, 0, 1, 0, 0, ],
-      [ 0, 0, 0, 0, 0, ], ],
+    [ [ 0, 1, 2, 0, 0, ],    # 0 talks to 1 once and 2 twice (weight 3)
+      [ 1, 0, 3, 0, 0, ],    # 1 talks to 0 once and 2 thrice (weight 4)
+      [ 2, 3, 0, 0, 0, ],    # 2 talks to 0 twice and 1 thrice (weight 5)
+      [ 0, 0, 1, 0, 0, ],    # 3 talks to 2 once (weight 1)
+      [ 0, 0, 0, 0, 0, ], ], # 4 talks to no-one (weight 0)
 ];
 
+diag 'Test weight LoL...';
 my $n = 0;
 for my $data (@$weight_dataset) {
     my $g = eval { Graph::Weighted->new() };
-    isa_ok $g, GW, $n;
+    isa_ok $g, GW, "weight $n";
+    # Populate the graph.
     eval { $g->populate($data) };
     print $@ if $@;
     ok !$@, "populate weight data $n";
+    # Know vertex weights.
     my $g_weight = 0;
     for my $vertex ($g->vertices()) {
         my $v_weight = $g->get_weight($vertex);
         $g_weight += $v_weight;
+        # Know edge weights.
+        for my $neighbor ($g->neighbors($vertex)) {
+            next unless $g->has_edge($vertex, $neighbor);
+            my $ew = $g->get_weight([$vertex, $neighbor]);
+            ok $ew, "edge weight ($vertex, $neighbor): $ew";
+        }
     }
     my $w = _weight_of($data);
-    is $g_weight, $w, "weight: $g_weight = $w";
+    is $g_weight, $w, "vertex weight: $g_weight = $w";
+
     $n++;
 }
 
@@ -47,24 +57,32 @@ my $magnitude_dataset = [
       [ 1, 1, 1, 1, 0, ], ],
 ];
 
+diag 'Test magnitude LoL...';
 $n = 0;
 for my $data (@$magnitude_dataset) {
     my $g = eval { Graph::Weighted->new() };
-    isa_ok $g, GW, $n;
+    isa_ok $g, GW, "magnitude $n";
     eval { $g->populate($data, '', 'magnitude') };
     print $@ if $@;
     ok !$@, "populate magnitude data $n";
     my $g_weight = 0;
+    # Know vertex weights.
     for my $vertex ($g->vertices()) {
         my $v_weight = $g->get_attr($vertex, 'magnitude');
         $g_weight += $v_weight;
+        # Know edge weights.
+        for my $neighbor ($g->neighbors($vertex)) {
+            next unless $g->has_edge($vertex, $neighbor);
+            my $ew = $g->get_weight([$vertex, $neighbor], 'magnitude');
+            ok $ew, "edge magnitude ($vertex, $neighbor): $ew";
+        }
     }
     my $w = _weight_of($data);
     is $g_weight, $w, "magnitude: $g_weight = $w";
     $n++;
 }
 
-# Populate with both weight and magnitude.
+diag 'Test both weight and magnitude LoL...';
 {
     my $g = eval { Graph::Weighted->new() };
     isa_ok $g, GW, 'weight and magnitude';
@@ -89,13 +107,54 @@ for my $data (@$magnitude_dataset) {
     }
     $w = _weight_of($magnitude_dataset->[-1]);
     is $g_weight, $w, "magnitude: $g_weight = $w";
+
+    for my $e ($g->edges) {
+        my $w = $g->get_weight($e, 'weight') || 0;
+        my $m = $g->get_weight($e, 'magnitude') || 0;
+        ok defined($w) && defined($m), "edge attributes: @$e = $w, $m";
+    }
 }
 
+=pod
+$weight_dataset = [
+    {}, # No nodes
+    { 0 => {} }, # 1 node, no edges
+    { 0 => {0 => 1}, 1 => {1 => 1} }, # 2 nodes of self-edges
+    # Same as LoL above but with alpha key names:
+    { a => {b=>2,c=>1}, b => {a=>3,c=>1}, c => {a=>3,b=>2}, d => {c=>1}, e => {a=>1,b=>1,c=>1,d=>1} },
+];
+diag 'Test weight HoH...';
+$n = 0;
+for my $data (@$weight_dataset) {
+    my $g = eval { Graph::Weighted->new() };
+    isa_ok $g, GW, "weight $n";
+    eval { $g->populate($data) };
+    print $@ if $@;
+    ok !$@, "populate weight data $n";
+    my $g_weight = 0;
+    for my $vertex ($g->vertices()) {
+        my $v_weight = $g->get_weight($vertex);
+        $g_weight += $v_weight;
+    }
+    my $w = _weight_of($data);
+    is $g_weight, $w, "weight: $g_weight = $w";
+    $n++;
+}
+=cut
+
+# Return total sum of a 2D numeric value data structure.
 sub _weight_of {
     my $data = shift;
     my $weight = 0;
-    for my $i (@$data) {
-        $weight += $_ for @$i;
+    if (ref $data eq 'ARRAY') {
+        for my $i (@$data) {
+            $weight += $_ for @$i;
+        }
+    }
+    elsif (ref $data eq 'HASH') {
+        for my $i (keys %$data) {
+            $weight += $_ for values %$i;
+        }
     }
     return $weight;
 }
